39dc4a8abc9cbc5bb8ecd5e3e0d63fcbf9d04a03
Olivia Cubela <oliviacubela@gmail.com>
2020-09-19 03:30:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "started my server using shotgun!"
+    "started my server using shotgun! testing testing testing"
   end
 
 end
